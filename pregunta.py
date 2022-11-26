548ea25b6b0026447db9237e7cbd9bf532ddd01b
acastrillonv <48304499+acastrillonv@users.noreply.github.com>
2022-11-26 09:56:44 -0500

    Primer intento

--- pregunta.py
+++ pregunta.py
@@ -16,5 +16,44 @@ def clean_data():
     #
     # Inserte su código aquí
     #
+    df = df.drop(df.columns[0],axis = 1)
+    df.drop_duplicates(inplace=(True))
+    df.dropna(inplace=(True))
+
+    lowerCase_columns = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio']
+    for column in lowerCase_columns:
+        df[column] = df[column].apply(lambda x: x.lower())
+    
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower())
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(check_dates)
+    df['monto_del_credito'] = df['monto_del_credito'].apply(check_prices)
+    df['línea_credito'] = df['línea_credito'].apply(check_credits)
+
+    df['idea_negocio'].replace({" ": '_'}, inplace=(True),  regex=True)
+    df['idea_negocio'].replace({"-": '_'}, inplace=(True),  regex=True)	
+    df['idea_negocio'].replace({"_": ' '}, inplace=(True),  regex=True)
+
+    df['barrio'].replace({" ": '_'}, inplace=(True),  regex=True)
+    df['barrio'].replace({"-": '_'}, inplace=(True),  regex=True)
+    df['barrio'].replace({"_": ' '}, inplace=(True),  regex=True)	
+
+    df.drop_duplicates(inplace=(True))
 
     return df
+
+def check_prices(price):
+    if(price[0] == '$'):
+        price = price[2:-3]
+        price = price.replace(',', '')
+    return price
+
+def check_credits(credit):
+    credit = credit.replace('_',' ')
+    credit = credit.replace('-',' ')
+    return credit
+
+def check_dates(date):
+    if (date[1] == '/') or (date[2] == '/'):
+           partes = date.split("/")
+           date = "/".join(reversed(partes))
+    return date
